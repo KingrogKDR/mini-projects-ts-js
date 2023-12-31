7076115413bf42a1b2c9ce2101d5502e40d16b13
KingrogKDR <149085408+KingrogKDR@users.noreply.github.com>
2023-12-31 14:47:21 +0000

    typescript added

---
+++ to-do-list/index.ts
@@ -0,0 +1,14 @@
+function addTask(){
+    let taskInput = document.getElementById("task-input") as HTMLInputElement;
+    let taskList = document.getElementById("todo-list");
+
+    if(taskInput.value !== ""){
+        const newEle = document.createElement("task");
+        newEle.textContent = taskInput.value;
+        taskList?.appendChild(newEle);
+        taskInput.value = "";
+    }
+}
+
+const addEle = document.getElementById("add-button");
+addEle?.addEventListener("click", addTask);
